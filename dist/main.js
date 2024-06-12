2cbcccc99422af8c54e6223483d4b3174a0f1f8a
Ricardo Tejo <ricardotejo@gmail.com>
2024-06-12 10:27:40 -0500

    fix PR comment

--- dist/main.js
+++ dist/main.js
@@ -23148,11 +23148,11 @@ async function main$1() {
 	const baselcov = baseRaw && (await parse$2(baseRaw));
 	const body = diff(lcov, baselcov, options).substring(0, MAX_COMMENT_CHARS);
 
-	if (shouldDeleteOldComments) {
-		await deleteOldComments(githubClient, options, github_1);
-	}
-
 	if (github_1.eventName === "pull_request") {
+		if (shouldDeleteOldComments) {
+			await deleteOldComments(githubClient, options, github_1);
+		}
+
 		await githubClient.issues.createComment({
 			repo: github_1.repo.repo,
 			owner: github_1.repo.owner,
